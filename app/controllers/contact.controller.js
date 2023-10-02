de0b3d77c2b3e73dc08136dc72393418203952f2
Phan Dai Cat <phandaicat12032002@gmail.com>
2023-10-02 10:24:59 +0700

    Cài đặc hàm create , findAll , findOne

--- app/controllers/contact.controller.js
+++ app/controllers/contact.controller.js
@@ -20,12 +20,41 @@ exports.create = async (req, res, next) => {
   }
 };
 
-exports.findAll = (req, res) => {
-  res.json({ message: "findAll handler" });
+exports.findAll = async (req, res, next) => {
+  let documents = [];
+
+  try {
+    const contactService = new ContactService(MongoDB.client);
+    const { name } = req.query;
+    if (name) {
+      documents = await contactService.findByName(name);
+    } else {
+      documents = await contactService.find({});
+    }
+  } catch (error) {
+    console.log(">>> err", error);
+    return next(
+      new ApiError(500, "An error occurred while creating the contact")
+    );
+  }
+
+  return res.send(documents);
 };
 
-exports.findOne = (req, res) => {
-  res.json({ message: "findOne handler" });
+exports.findOne = async (req, res, next) => {
+  try {
+    const contactService = new ContactService(MongoDB.client);
+    const document = await contactService.findById(req.params.id);
+    if (!document) {
+      return next(new ApiError(404, "Ket noi that bai"));
+    }
+    return res.send(document);
+  } catch (error) {
+    console.log(">>> err", error);
+    return next(
+      new ApiError(500, "An error occurred while creating the contact")
+    );
+  }
 };
 
 exports.update = (req, res) => {
